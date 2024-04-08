e720cbc75a7a575955b85fc5feb1fb01db339779
Oktay <oktay.parlak.12@gmail.com>
2024-04-09 01:03:49 +0300

    Refactor validateId middleware, remove commented out code

--- server/src/middlewares/validateId.js
+++ server/src/middlewares/validateId.js
@@ -1,30 +1,5 @@
 const uuid = require('uuid');
 
-// function validateId(req, res, next) {
-//   try {
-//     const { id, userId, courseId } = req.params;
-//     for (let item in req.params) {
-//       console.log(item);
-//     }
-//     const params = [id, userId, courseId].filter(Boolean);
-
-//     if (params.length !== 1) {
-//       return res.status(400).json({ error: { message: 'Invalid parameters' } });
-//     }
-
-//     const [param] = params;
-//     if (!uuid.validate(param)) {
-//       return res.status(400).json({ error: { message: 'Invalid id' } });
-//     }
-
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// }
-
-// module.exports = validateId;
-
 module.exports = (req, res, next) => {
   try {
     for (let item in req.params) {
